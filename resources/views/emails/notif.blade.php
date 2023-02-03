db8163b77e682a91b1e981822c6e0c409da8c98d
daffaW <44244449+daffawrdhn@users.noreply.github.com>
2023-02-03 10:47:19 +0700

    Update notif.blade.php

--- resources/views/emails/notif.blade.php
+++ resources/views/emails/notif.blade.php
@@ -4,9 +4,30 @@
     <title>E-ticket - Email Notification</title>
 </head>
 <body>
-    <h1>{{ $details['subject'] }}</h1>
+    <table style="width: 100%;">
+        <tr>
+            <td style="text-align: right;">
+                <h2 style="margin: 0;">E-ticket</h2>
+                <p style="margin: 0; font-size: 12px;">Email Notification</p>
+            </td>
+        </tr>
+    </table>
+
+    <hr style="border-top: 1px solid #ccc;">
+
+    <h3>{{ $details['subject'] }}</h3>
     <p>{{ $details['body'] }}</p>
-   
-    <p>Thank you</p>
+
+    <hr style="border-top: 1px solid #ccc;">
+
+    <p style="font-size: 12px;">
+        Sent by: E-Ticket - MT HCIS<br>
+        Altira Office Tower kav. 85 Jl. Yos Sudarso, Tanjung Priok District, Kota Jakarta Utara, 14350, Indonesia<br>
+        mthcis@outlook.com
+    </p>
+
+    <p style="font-size: 12px; color: #999;">
+        *This is an automated notification. Please do not reply to this email.
+    </p>
 </body>
 </html>
