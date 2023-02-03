8ae5b68e175b4b4e5593515032c98cda8ece1848
daffaW <44244449+daffawrdhn@users.noreply.github.com>
2023-02-03 09:12:28 +0700

    email blade template

---
+++ resources/views/emails/notif.blade.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <title>{{ $subject }}</title>
+  </head>
+  <body>
+    <table style="width:100%;">
+      <tr>
+        <td style="padding: 10px;">
+          <h1 style="color: #1c75bb;">{{ $subject }}</h1>
+        </td>
+      </tr>
+      <tr>
+        <td style="padding: 10px;">
+          {{ $body }}
+        </td>
+      </tr>
+      <tr>
+        <td style="padding: 10px;">
+          <p style="font-size: 12px;">This is an automated email, please do not reply.</p>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
