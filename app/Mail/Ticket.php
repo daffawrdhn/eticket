5295710b56877c9b6bdbc20887e8585ca94859e8
daffaW <44244449+daffawrdhn@users.noreply.github.com>
2023-02-08 15:14:07 +0700

    email queue

--- app/Mail/Ticket.php
+++ app/Mail/Ticket.php
@@ -9,7 +9,8 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class Ticket extends Mailable
+class Ticket extends Mailable implements ShouldQueue
+
 {
     use Queueable, SerializesModels;
 
