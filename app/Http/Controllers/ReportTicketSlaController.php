57ebe52adf1420d0ffb48a1a9c203449f709b352
paseo126 <93853731+paseo126@users.noreply.github.com>
2023-02-21 21:29:41 +0700

    Update ReportTicketSlaController.php

--- app/Http/Controllers/ReportTicketSlaController.php
+++ app/Http/Controllers/ReportTicketSlaController.php
@@ -25,11 +25,34 @@ class ReportTicketSlaController extends BaseController
                 $isStatusTicket = TicketStatusHistory::where('ticket_id', $ticket->ticket_id)->first();
 
                 $submited = TicketStatusHistory::where('ticket_id', $isStatusTicket->ticket_id)->where('status_after', 1)->first();
+                $approve1 = TicketStatusHistory::where('ticket_id', $isStatusTicket->ticket_id)->where('status_after', 2)->first();
+                $approve2 = TicketStatusHistory::where('ticket_id', $isStatusTicket->ticket_id)->where('status_after', 3)->first();
+                $approve3 = TicketStatusHistory::where('ticket_id', $isStatusTicket->ticket_id)->where('status_after', 4)->first();
+                $finalApprove = TicketStatusHistory::where('ticket_id', $isStatusTicket->ticket_id)->where('status_after', 5)->first();
+                $reject = TicketStatusHistory::where('ticket_id', $isStatusTicket->ticket_id)->where('status_after', 6)->first();
+                $inProgress = TicketStatusHistory::where('ticket_id', $isStatusTicket->ticket_id)->where('status_after', 7)->first();
+                $done = TicketStatusHistory::where('ticket_id', $isStatusTicket->ticket_id)->where('status_after', 8)->first();
 
+
+                $isSubmited = $submited == null ? '-' : date('d F Y', strtotime($submited->created_at));
+                $isApprove1 = $approve1 == null ? '-' : date('d F Y', strtotime($approve1->created_at));
+                $isApprove2 = $approve2 == null ? '-' : date('d F Y', strtotime($approve2->created_at));
+                $isApprove3 = $approve3 == null ? '-' : date('d F Y', strtotime($approve3->created_at));
+                $isFinal = $finalApprove == null ? '-' : date('d F Y', strtotime($finalApprove->created_at));
+                $isReject = $reject == null ? '-' : date('d F Y', strtotime($reject->created_at));
+                $isInProgress = $inProgress == null ? '-' : date('d F Y', strtotime($inProgress->created_at));
+                $isDone = $done == null ? '-' : date('d F Y', strtotime($done->created_at));
 
                 $sla[] = [
                     'ticket_id' => $ticket->ticket_id,
-                    'time' => $submited->created_at
+                    'submited_date' => $isSubmited,
+                    'approve1_date' => $isApprove1,
+                    'approve2_date' => $isApprove2,
+                    'approve3_date' => $isApprove3,
+                    'final_approve_date' => $isFinal,
+                    'reject_date' => $isReject,
+                    'in_progress' => $isInProgress,
+                    'is_done' => $isDone,
                 ];
             }
 
