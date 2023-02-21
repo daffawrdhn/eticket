2515bb00fd2772f9927fca3fc323157e441fa2de
paseo126 <93853731+paseo126@users.noreply.github.com>
2023-02-21 23:50:54 +0700

    Update ReportTicketSlaController.php

--- app/Http/Controllers/ReportTicketSlaController.php
+++ app/Http/Controllers/ReportTicketSlaController.php
@@ -83,31 +83,14 @@ class ReportTicketSlaController extends BaseController
     public function dateInterval($startDate, $endDate)
     {
 
-        // hitung selisih waktu dalam jam
-        $diff = $startDate->diff($endDate);
-
-        // hitung jumlah hari kerja antara dua tanggal
-        $days = 0;
-        $interval = new DateInterval('P1D'); // interval 1 hari
-        $period = new DatePeriod($startDate, $interval, $endDate);
-
-        foreach ($period as $date) {
-        if ($date->format('N') <= 5) { // hitung hari kerja Senin hingga Jumat
-            $days++;
-        }
-        }
-
-        $hours = ($days * 8) + ($diff->h - 1); // asumsi 1 jam istirahat per hari
-        $total_seconds = ($hours * 3600) + ($diff->i * 60) + $diff->s; // total detik
-        $days = floor($total_seconds / (24 * 60 * 60)); // hitung jumlah hari
-        $total_seconds %= (24 * 60 * 60); // sisa detik setelah hari dihitung
-        $hours = floor($total_seconds / 3600); // hitung jumlah jam
-        $total_seconds %= 3600; // sisa detik setelah jam dihitung
-        $minutes = floor($total_seconds / 60); // hitung jumlah menit
-        $seconds = $total_seconds % 60; // hitung sisa detik
-
-        // output selisih waktu dalam format hari:jam:menit:detik
-        return $days . ':' . $hours . ':' . $minutes .':'.$seconds;
+        $diffInDays = $startDate->diffInDaysFiltered(function (Carbon $date) {
+            return $date->isWeekday();
+        }, $endDate);
+        
+        // Menghitung selisih waktu
+        $diffInHours = $startDate->diffInHours($endDate) % 24;
+        
+        return $diffInDays . " hari " . $diffInHours . " jam ";
     }
 }
 
