897163bdb51145a67f23d27e4fccd9d0d9734ab2
paseo126 <93853731+paseo126@users.noreply.github.com>
2023-02-21 23:59:10 +0700

    Update ReportTicketSlaController.php

--- app/Http/Controllers/ReportTicketSlaController.php
+++ app/Http/Controllers/ReportTicketSlaController.php
@@ -84,7 +84,7 @@ class ReportTicketSlaController extends BaseController
     {
 
         $diffInDays = $startDate->diffInDaysFiltered(function (Carbon $date) {
-            return !$date->isWeekday() && !$date->isHoliday();
+            return $date->isWeekday();
         }, $endDate);
         
         // Menghitung selisih waktu
