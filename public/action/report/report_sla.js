9de423d60a2b12a312f72c04f8238d81ec1269b4
paseo126 <93853731+paseo126@users.noreply.github.com>
2023-02-22 02:17:44 +0700

    Update report_sla.js

--- public/action/report/report_sla.js
+++ public/action/report/report_sla.js
@@ -32,8 +32,7 @@ $(document).ready(function () {
 
 
     $('#regional-select').on("select2:selecting", function(e) { 
-        e.preventDefault();
-        var value = e.params.id;
+        var value = e.params.data['id'];
         console.log(value);
      });
     
