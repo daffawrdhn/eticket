9fd98aba1c72f285710bb2687b70606486d7d7b8
paseo126 <93853731+paseo126@users.noreply.github.com>
2023-02-21 16:46:44 +0700

    Update ReportTicketSlaController.php

--- app/Http/Controllers/ReportTicketSlaController.php
+++ app/Http/Controllers/ReportTicketSlaController.php
@@ -27,21 +27,57 @@ class ReportTicketSlaController extends BaseController
                 foreach($isStatusTicket as $status){
                     $data[] = $status->status_after;
 
-                    // if ($status->status_after == 1) {
-                    //     $submited = date('d F Y', strtotime($status->created_at));
-                    // }else if($status->status_after == 2){
-                    //     $approval1 = date('d F Y', strtotime($status->created_at));
-                    // }else if($status->status_after == 3){
-                    //     $approval2 = date('d F Y', strtotime($status->created_at));
-                    // }else if($status->status_after == 4){
-                    //     $approval3 = date('d F Y', strtotime($status->created_at));
-                    // }else if($status->status_after == 5){
-                    //     $finalApprov = date('d F Y', strtotime($status->created_at));
-                    // }else if($status->status_after == 6){
-                    //     $reject = date('d F Y', strtotime($status->created_at));
-                    // }else if($status->status_after == 7){
-                    //     $onProcess = date('d F Y', strtotime($status->created_at));
-                    // }
+                    if ($status->status_after == 1) {
+                        $submited = date('d F Y', strtotime($status->created_at));
+                    }else{
+                        $submited = 0;
+                    }
+                    
+                    if($status->status_after == 2){
+                        $approval1 = date('d F Y', strtotime($status->created_at));
+                    }else{
+                        $approval1 = 0;
+                    }
+                    
+                    if($status->status_after == 3){
+                        $approval2 = date('d F Y', strtotime($status->created_at));
+                    }else{
+                        $approval2 = 0;
+                    }
+                    
+                    if($status->status_after == 4){
+                        $approval3 = date('d F Y', strtotime($status->created_at));
+                    }else{
+                        $approval3 = 0;
+                    }
+                    
+                    if($status->status_after == 5){
+                        $finalApprov = date('d F Y', strtotime($status->created_at));
+                    }else{
+                        $finalApprov = 0;
+                    }
+                    
+                    if($status->status_after == 6){
+                        $reject = date('d F Y', strtotime($status->created_at));
+                    }else{
+                        $reject = 0;
+                    }
+                    
+                    if($status->status_after == 7){
+                        $onProcess = date('d F Y', strtotime($status->created_at));
+                    }else{
+                        $onProcess = 0;
+                    }
+                    
+                    $data[] = [
+                        'submited' => $submited,
+                        'approval1' => $approval1,
+                        'approval2' => $approval2,
+                        'approval3' => $approval3,
+                        'finalApprov' => $finalApprov,
+                        'onProcess' => $onProcess,
+                    ];
+                
                 }
 
                 $datas[] = [
